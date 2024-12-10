34c9a691fc3d52b978ffec2d7cbae2108f0ecdcb
tanim0299 <tanimchy417@gmail.com>
2024-12-10 16:47:18 +0600

    status_icon_updated

--- resources/views/admin/menu_label/show.blade.php
+++ resources/views/admin/menu_label/show.blade.php
@@ -113,8 +113,8 @@
                                 </td>
                                 <td>
                                     <div class="checkbox-wrapper-51">
-                                        <input onchange="return changeMenuStatus({{$v->id}})" id="cbx-51" type="checkbox" @if($v->status == 1) checked @endif>
-                                        <label class="toggle" for="cbx-51">
+                                        <input onchange="return changeMenuStatus({{$v->id}})" id="cbx-51-'.$row->id.'" type="checkbox" @if($v->status == 1) checked @endif>
+                                        <label class="toggle" for="cbx-51-'.$row->id.'">
                                           <span>
                                             <svg viewBox="0 0 10 10" height="10px" width="10px">
                                               <path d="M5,1 L5,1 C2.790861,1 1,2.790861 1,5 L1,5 C1,7.209139 2.790861,9 5,9 L5,9 C7.209139,9 9,7.209139 9,5 L9,5 C9,2.790861 7.209139,1 5,1 L5,9 L5,1 Z"></path>
